5c14dc4199ea272f3f98ad4cc2fc760006d79af3
Felix Lanas <felix.lanas@gmail.com>
2021-04-08 18:58:32 +0200

    First Commit

--- frames/MessagesFrame.py
+++ frames/MessagesFrame.py
@@ -8,14 +8,15 @@ MAX_MESSAGE_WIDTH = 1300
 
 
 class MessagesFrame(ttk.Frame):
-    def __init__(self, container, **kwargs):
+    def __init__(self, container, background, **kwargs):
         super().__init__(container, **kwargs)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         self.messages_frames = []
 
-        self.canvas = tk.Canvas(self)
+        # -- CANVAS + SCROLL FRAME --
+        self.canvas = tk.Canvas(self, background=background)
         self.scrollable_frame = ttk.Frame(self.canvas)
         self.scrollable_window = self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor='nw')
         self.scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
@@ -23,15 +24,19 @@ class MessagesFrame(ttk.Frame):
 
         self.scrollbar.grid(row=0, column=1, sticky='NS')
         self.canvas.grid(row=0, column=0, sticky='NSEW')
+        # --
 
+        # -- EVENTS --
         self.scrollable_frame.bind('<Configure>', self._configure_scroll_region)
         self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
         self.canvas.bind('<Configure>', self._configure_window_size)
-
+        # --
 
     def add_new_message(self, new_message):
-        print('Aquí estoy')
-        new_message_frame = MessageFrame(self.scrollable_frame, new_message['date'], new_message['message'])
+        new_message_frame = MessageFrame(self.scrollable_frame,
+                                         new_message['date'],
+                                         new_message['message'],
+                                         style='Messages.TFrame')
         new_message_frame.grid(sticky='EW', padx=(10, 50), pady=10)
         self.messages_frames.append(new_message_frame)
 
@@ -39,7 +44,6 @@ class MessagesFrame(ttk.Frame):
         self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def _on_mousewheel(self, event):
-        print(event.delta // 120)
         self.canvas.yview_scroll(event.delta*-1, 'units')
 
     def _configure_window_size(self, event):
@@ -56,22 +60,30 @@ class MessageFrame(ttk.Frame):
         self.avatar_image = Image.open('assets/' + random.choice(('luigi.png', 'mario.png', 'peach.jpg')))
         self.avatar_image.thumbnail((25, 25), Image.ANTIALIAS)
         self.avatar_photo = ImageTk.PhotoImage(self.avatar_image)
-        self.avatar_label = ttk.Label(self, image=self.avatar_photo)
-        self.avatar_label.grid(row=0, column=0, rowspan=2, sticky='NEW', padx=(0, 10), pady=(5, 0))
+        self.avatar_label = ttk.Label(self,
+                                      image=self.avatar_photo,
+                                      anchor='center',
+                                      style='Avatar.TLabel')
+        self.avatar_label.grid(row=0,
+                               column=0,
+                               rowspan=2,
+                               sticky='NESW',
+                               padx=5)
 
         date_string = datetime.datetime.fromtimestamp(date).strftime("%d-%m-%Y %H:%M:%S")
         date_label = ttk.Label(self,
                                text=date_string,
                                anchor='w',
                                justify='left',
-                               )
+                               style='Time.TLabel')
         date_label.grid(row=0, column=1, sticky='EW')
         self.message_label = ttk.Label(self,
                                        text=message_text,
                                        anchor='w',
                                        justify='left',
-                                       wraplength=(container.winfo_width()-50)
-                                       )
+                                       wraplength=(container.winfo_width()-50),
+                                       style='Message.TLabel')
+
         self.message_label.grid(row=1, column=1, sticky='EW')
 
         def reconfigure_message_label(event):
--- frames/chat.py
+++ frames/chat.py
@@ -8,29 +8,58 @@ messages_already_shown = []
 
 
 class Chat(ttk.Frame):
-    def __init__(self, container, **kwargs):
+    def __init__(self, container, background, **kwargs):
         super().__init__(container, **kwargs)
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        self.messages_frame = MessagesFrame(self)
-        self.messages_frame.grid(row=0, column=0, stick='NSEW', pady=5)
+        # -- MESSAGES FRAME --
+        self.messages_frame = MessagesFrame(self,
+                                            background,
+                                            style='Messages.TFrame')
+        self.messages_frame.grid(row=0, column=0, stick='NSEW', pady=10)
+        # --
 
-        input_frame = ttk.Frame(self, padding=10)
+        # -- INPUTS AND BUTTONS --
+        input_frame = ttk.Frame(self, padding=10, style='Controls.TFrame')
         input_frame.grid(row=1, column=0, sticky="EW")
 
-        message_fetch = ttk.Button(
-            input_frame,
-            text="Fetch",
-            command=self.get_messages
-        )
+        self.message_input = tk.Text(input_frame, height=3)
+        self.message_input.pack(expand=True, fill='both', side='left', padx=(0, 10))
+        self.message_input.focus()
+
+        message_submit = ttk.Button(input_frame,
+                                    text='Send',
+                                    style='SendButton.TButton',
+                                    command=self.post_message)
+        message_submit.pack(expand='True')
+
+        message_fetch = ttk.Button(input_frame,
+                                   text="Fetch",
+                                   style='FetchButton.TButton',
+                                   command=self.get_messages)
         message_fetch.pack()
+        # --
+
+        # -- EVENTS --
+        self.message_input.bind('<Return>', self.post_message)
+        self.message_input.bind('<KP_Enter>', self.post_message)
+
+    def post_message(self, *args):
+        body = self.message_input.get(1.0, tk.END+"-1c")
+        requests.post('http://167.99.63.70/message', json={'message': body})
+        self.message_input.delete(1.0, tk.END+"-1c")
+        self.after(50, lambda: self.messages_frame.canvas.yview_moveto('1.0'))
+        self.get_messages()
+
+        return 'break'
 
     def get_messages(self):
         global messages
         messages = requests.get("http://167.99.63.70/messages").json()
         self.update_messages_widgets()
+        self.after(150, lambda: self.messages_frame.canvas.yview_moveto('1.0'))
 
     def update_messages_widgets(self):
         global messages, messages_already_shown
